64fc673622b5e82ce681e57c36049d6ab0786ced
user <jakypjumagulov080gmail.com>
2024-03-11 23:22:29 +0600

    error import_react3

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,60 +1,201 @@
-import React, { useState } from 'react'
+// import React, { useState } from 'react'
 
-import { Link, NavLink } from 'react-router-dom'
+// import { Link, NavLink } from 'react-router-dom'
 
-function Header() {
-    const [activeMenu, setActiveMenu] = useState(false)
+// function Header() {
+//     const [activeMenu, setActiveMenu] = useState(false)
 
-    const handleMenuClick = (index) => {
-        setActiveMenu(index)
-        setActiveMenu(!activeMenu)
-    }
+//     const handleMenuClick = (index) => {
+//         setActiveMenu(index)
+//         setActiveMenu(!activeMenu)
+//     }
+
+//   return (
+//     <header className='header'>
+//       <div className='container'>
+//         <div className='logo'>
+//             <Link to='/'>Tenders.kg</Link>
+//         </div>
+//         <div className='header-menu'>
+//             <nav>
+//                 <ul>
+//                     <li>
+//                         <NavLink onClick={() => handleMenuClick(0)} className={activeMenu === 0 ? "active" : ""} to='/'>Главная</NavLink>
+//                     </li>
+//                     <li>
+//                         <NavLink onClick={() => handleMenuClick(1)} className={activeMenu === 1 ? "active" : ""} to='/contest'>Конкурсы</NavLink>
+//                     </li>
+//                     <li>
+//                         <NavLink onClick={() => handleMenuClick(2)} className={activeMenu === 2 ? "active" : ""} to='/auction'>Аукционы</NavLink>
+//                     </li>
+//                     <li>
+//                         <NavLink onClick={() => handleMenuClick(3)} className={activeMenu === 3 ? "active" : ""} to='/tender'>Тендеры</NavLink>
+//                     </li>
+//                     <li>
+//                         <NavLink onClick={() => handleMenuClick(4)} className={activeMenu === 4 ? "active" : ""} to='/grant'>Гранты</NavLink>
+//                     </li>
+//                     <li>
+//                         <NavLink onClick={() => handleMenuClick(5)} className={activeMenu === 5 ? "active" : ""} to='/other'>Ище 
+//                             <svg width="14" height="9" viewBox="0 0 14 9" fill="none" xmlns="http://www.w3.org/2000/svg">
+//                             <path d="M12 1H2L7 7.5L12 1Z" fill="white" stroke="white"/>
+//                             </svg>
+//                         </NavLink>
+//                     </li>
+//                 </ul>
+//             </nav>
+//             <div className='header-btns'>
+//                 <button>
+//                     <NavLink to='/authentication'>Войти</NavLink>
+//                 </button>
+//                 <button>
+//                     <NavLink to='/registration'>Регистрация</NavLink>
+//                 </button>
+//             </div>
+//         </div>
+//         <label htmlFor="burger" className='burger'>
+//             <input type="checkbox" id="burger" />
+//             <span></span>
+//             <span></span>
+//             <span></span>
+//         </label>
+//       </div>
+//     </header>
+//   )
+// }
+
+// export default Header
+
+
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Button from '@mui/material/Button';
+import MenuItem from '@mui/material/MenuItem';
+
+console.log(React.version);
+
+const pages = ['Products', 'Pricing', 'Blog'];
+
+function ResponsiveAppBar() {
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
+  const [anchorElUser, setAnchorElUser] = React.useState(null);
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
 
   return (
-    <header className='header'>
-      <div className='container'>
-        <div className='header-menu'>
-            <div className='logo'>
-                <Link to='/'>Tenders.kg</Link>
-            </div>
-            <nav>
-                <ul>
-                    <li>
-                        <NavLink onClick={() => handleMenuClick(0)} className={activeMenu === 0 ? "active" : ""} to='/'>Главная</NavLink>
-                    </li>
-                    <li>
-                        <NavLink onClick={() => handleMenuClick(1)} className={activeMenu === 1 ? "active" : ""} to='/contest'>Конкурсы</NavLink>
-                    </li>
-                    <li>
-                        <NavLink onClick={() => handleMenuClick(2)} className={activeMenu === 2 ? "active" : ""} to='/auction'>Аукционы</NavLink>
-                    </li>
-                    <li>
-                        <NavLink onClick={() => handleMenuClick(3)} className={activeMenu === 3 ? "active" : ""} to='/tender'>Тендеры</NavLink>
-                    </li>
-                    <li>
-                        <NavLink onClick={() => handleMenuClick(4)} className={activeMenu === 4 ? "active" : ""} to='/grant'>Гранты</NavLink>
-                    </li>
-                    <li>
-                        <NavLink onClick={() => handleMenuClick(5)} className={activeMenu === 5 ? "active" : ""} to='/other'>Ище 
-                            <svg width="14" height="9" viewBox="0 0 14 9" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M12 1H2L7 7.5L12 1Z" fill="white" stroke="white"/>
-                            </svg>
-                        </NavLink>
-                    </li>
-                </ul>
-            </nav>
-        </div>
-        <div className='header-btns'>
-            <button>
-                <NavLink to='/authentication'>Войти</NavLink>
-            </button>
-            <button>
-                <NavLink to='/registration'>Регистрация</NavLink>
-            </button>
-        </div>
-      </div>
-    </header>
-  )
+    <div className='container'>
+      <AppBar position="static" className='app-bar'>
+      <Container maxWidth="xl">
+        <Toolbar className='toolbar' disableGutters>
+          <div className='cont-row'>
+          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'left',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'left',
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: 'block', md: 'none' },
+              }}
+            >
+              {pages.map((page) => (
+                <MenuItem key={page} onClick={handleCloseNavMenu}>
+                  <Typography textAlign="center">{page}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+            <Typography
+              variant="h6"
+              noWrap
+              component="a"
+              href="#app-bar-with-responsive-menu"
+              sx={{
+              mr: 2,
+              display: { xs: 'none', md: 'flex' },
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            Tenders.kg
+          </Typography>
+          </div>
+          <div className='cont'>
+          <Typography
+            variant="h5"
+            noWrap
+            component="a"
+            href="#app-bar-with-responsive-menu"
+            sx={{
+              mr: 2,
+              display: { xs: 'flex', md: 'none' },
+              flexGrow: 1,
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            Tenders.kg
+          </Typography>
+          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+            {pages.map((page) => (
+              <Button
+                key={page}
+                onClick={handleCloseNavMenu}
+                sx={{ my: 2, color: 'white', display: 'block' }}
+              >
+                {page}
+              </Button>
+            ))}
+          </Box>
+          </div>
+        </Toolbar>
+      </Container>
+    </AppBar>
+    </div>
+  );
 }
-
-export default Header
+export default ResponsiveAppBar;
