5e3e0aca39f327395fd2de2e41d16b41e4d214cc
user <jakypjumagulov080gmail.com>
2024-03-15 14:51:38 +0600

    first home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import Layout from './components/layout/Layout'
 import { Route, Routes } from 'react-router-dom'
-import { Auction, Contest, Grant, NotFound, Tender } from './pages'
+import { Auction, Contest, Grant, Home, NotFound, Tender } from './pages'
 
 function App() {
   return (
     <div>
       <Routes>
         <Route path='/' element={<Layout />}>
+          <Route path='/' element={<Home />}/>
           <Route path='/auction' element={<Auction />}/>
           <Route path='/contest' element={<Contest />}/>
           <Route path='/grant' element={<Grant />}/>
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -20,157 +20,51 @@ import Dropdown from '@mui/joy/Dropdown';
 import InputBase from '@mui/material/InputBase';
 
 function Home() {
-  const SIZES = ['X-Small', 'Small', 'Medium', 'Large', 'X-Large'];
-  // const [size, setSize] = React.useState('Medium');
+  const SIZES = ['Тендеры', 'Конкурсы', 'Аукционы', 'Запросы коммерческих предложений', 'Запросы котировок', 'Запрос на выражение заинтересованности', 'Реализации', 'Запросы грантовых предложений', 'Прочие'];
 
   const [selectedIndex, setSelectedIndex] = React.useState(-1);
   return (
     <section className='section'>
       <div className='container'>
-        {/* <Autocomplete
-          disablePortal
-          id="combo-box-demo"
-          options={top100Films}
-          sx={{ width: 300, background: '#44ACE9' }}
-          renderInput={(params) => <TextField {...params} label="Показать фильтры"/>}
-        /> */}
-        {/* <Autocomplete
-  disablePortal
-  id="combo-box-demo"
-  options={top100Films}
-  sx={{}} // Удаление всех стилей
-  renderInput={(params) => <TextField {...params} label="Показать фильтры"/>}
-/> */}
-
-      {/* <Dropdown>
-        <MenuButton endDecorator={<ArrowDropDown />}>Size</MenuButton>
-        <Menu sx={{ minWidth: 160, '--ListItemDecorator-size': '24px' }}>
-        <ListDivider />
-        <ListItem nested>
-          <List aria-label="Font sizes">
-            {SIZES.map((item) => (
-              <MenuItem
-                key={item}
-                role="menuitemradio"
-                aria-checked={item === size ? 'true' : 'false'}
-                onClick={() => {
-                  setSize(item);
-                }}
-              >
-                <ListItemDecorator>
-                  {item === size && <ArrowRight />}
-                </ListItemDecorator>{' '}
-                {item}
-              </MenuItem>
-            ))}
-          </List>
-        </ListItem>
-      </Menu>
-    </Dropdown> */}
-    {/* <Dropdown>
-      <MenuButton endDecorator={<ArrowDropDown />}>{SIZES[selectedIndex]}</MenuButton>
-      <Menu sx={{ minWidth: 160, '--ListItemDecorator-size': '24px' }}>
-        <ListDivider />
-        <ListItem nested>
-          <List aria-label="Font sizes">
-            {SIZES.map((item, index) => (
-              <MenuItem
-                key={item}
-                role="menuitemradio"
-                aria-checked={index === selectedIndex ? 'true' : 'false'}
-                onClick={() => {
-                  setSelectedIndex(index);
-                }}
-              >
-                <ListItemDecorator>
-                  {index === selectedIndex && <ArrowRight />}
-                </ListItemDecorator>{' '}
-                {item}
-              </MenuItem>
-            ))}
-          </List>
-        </ListItem>
-      </Menu>
-    </Dropdown> */}
-    {/* <Dropdown>
-      <MenuButton endDecorator={<ArrowDropDown />}>selectedIndex ? "dd" : {SIZES[selectedIndex]}</MenuButton>
-      <Menu sx={{ minWidth: 160, '--ListItemDecorator-size': '24px' }}>
-        <ListDivider />
-        <ListItem nested>
-          <List aria-label="Font sizes">
-            {SIZES.map((item, index) => (
-              <MenuItem
-                key={item}
-                role="menuitemradio"
-                aria-checked={index === selectedIndex ? 'true' : 'false'}
-                onClick={() => {
-                  setSelectedIndex(index);
-                }}
-              >
-                <ListItemDecorator>
-                  {index === selectedIndex && <ArrowRight />}
-                </ListItemDecorator>{' '}
-                {item}
-              </MenuItem>
-            ))}
-          </List>
-        </ListItem>
-      </Menu>
-    </Dropdown> */}
-    <Dropdown>
-      <MenuButton endDecorator={<ArrowDropDown />}>
-        {selectedIndex !== -1 ? SIZES[selectedIndex] : "dd"}
-      </MenuButton>
-      <Menu sx={{ minWidth: 160, '--ListItemDecorator-size': '24px' }}>
-        <ListDivider />
-        <ListItem nested>
-          <List aria-label="Font sizes">
-            {SIZES.map((item, index) => (
-              <MenuItem
-                key={item}
-                role="menuitemradio"
-                aria-checked={index === selectedIndex ? 'true' : 'false'}
-                onClick={() => {
-                  setSelectedIndex(index);
-                }}
-              >
-                <ListItemDecorator>
-                  {index === selectedIndex && <ArrowRight />}
-                </ListItemDecorator>{' '}
-                {item}
-              </MenuItem>
-            ))}
-          </List>
-        </ListItem>
-      </Menu>
-    </Dropdown>
+        <Dropdown>
+            <MenuButton  endDecorator={<ArrowDropDown />} sx={{background: '#44ACE9', color: '#fff', "&:hover": { backgroundColor: "#44ACE9" }}}>
+              {selectedIndex !== -1 ? SIZES[selectedIndex] : "Показать фильтры"}
+            </MenuButton>
+            <Menu sx={{minWidth: 160, '--ListItemDecorator-size': '24px' }}>
+              <ListItem nested>
+                <List aria-label="Font sizes">
+                  {SIZES.map((item, index) => (
+                  <MenuItem
+                    key={item}
+                    role="menuitemradio"
+                    aria-checked={index === selectedIndex ? 'true' : 'false'}
+                      onClick={() => {
+                      setSelectedIndex(index);
+                    }}
+                  >
+                    <ListItemDecorator>
+                     {index === selectedIndex && <ArrowRight />}
+                    </ListItemDecorator>{' '}
+                      {item}
+                  </MenuItem>
+                ))}
+              </List>
+            </ListItem>
+          </Menu>
+        </Dropdown>
         <div className='search-main'>
-          {/* <Box
-            sx={{
-              width: 500,
-              maxWidth: '100%',
-            }}
-          >
-            <TextField fullWidth label="fullWidth" id="fullWidth" />
-          </Box> */}
           <InputBase
-        sx={{ ml: 1, flex: 1, border: '1px solid black', outline: 'none', borderRadius: '3px', width: 400 }}
-        placeholder="Search Google Maps"
-        inputProps={{ 'aria-label': 'search google maps' }}
-      />
-          <Stack spacing={1} direction="row">
-            <Button variant="contained">Contained</Button>
+            sx={{mr: -2,flex: 1, border: '1px solid #44ACE9', outline: 'none', borderRadius: '3px', padding: '1px 20px' }}
+            placeholder="Поиск....."
+            inputProps={{ 'aria-label': 'search google maps' }}
+          />
+          <Stack  spacing={1} direction="row">
+            <Button sx={{background: '#44ACE9', color: '#fff', "&:hover": { backgroundColor: "#44ACE9" },boxShadow: "none", borderRadius: '0 3px 3px 0' }} variant="contained">Искать</Button>
           </Stack>
         </div>
       </div>
     </section>
   )
 }
-
-const top100Films = [
-  { label: 'The Shawshank Redemption', year: 1994 },
-  { label: 'The Godfather', year: 1972 },
-  { label: 'The Godfather: Part II', year: 1974 },
-];
 
 export default Home
